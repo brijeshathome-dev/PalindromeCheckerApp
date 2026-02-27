717cf71b303c3191005a8888de7f4e8d285e5549
Brijesh R S <brijeshathome@gmail.com>
2026-02-27 15:24:52 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,26 @@
 public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("   WELCOME TO PALINDROME APP   ");
+        System.out.println("=================================");
+
+        // Hardcoded word
+        String word = "madam";
+
+        String reversed = "";
+
+        // Reverse the word
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        // Check palindrome
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
+    }
 }
